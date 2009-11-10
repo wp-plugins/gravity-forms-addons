0ac9f4dd60c41f85d9b345f4be043e8c87b15039
katzwebdesign <katzwebdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-10 22:30:34 +0000

    Gravity Forms Addons 1.2 - Compatibility for GF 1.3
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-addons/trunk@172174 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravity-forms-addons.php
+++ gravity-forms-addons.php
@@ -2,15 +2,18 @@
 /*
 Plugin Name: Gravity Forms Addons
 Plugin URI: http://www.seodenver.com/gravity-forms-addons/
-Description: Add functionality to the great Gravity Forms plugin. Features: Expand all of the 'Add Fields' boxes at once
+Description: Add functionality and usability to the great Gravity Forms plugin.
 Author: Katz Web Services, Inc.
-Version: 1.1
+Version: 1.2
 Author URI: http://www.katzwebservices.com
 */
 
 /*
 
 Versions:
+
+= 1.2 = 
+* Compatibility with Gravity Forms 1.3
 
 = 1.1 =
 * Added Edit link to Entries page to directly edit an entry
@@ -24,7 +27,11 @@ Versions:
 // Get Gravity Forms over here!
 @include_once(WP_PLUGIN_DIR . "/gravityforms/gravityforms.php");
 @include_once(WP_PLUGIN_DIR . "/gravityforms/forms_model.php");
-$Forms = new RGForms();  $RG = new RGFormsModel();
+@include_once(WP_PLUGIN_DIR . "/gravityforms/common.php"); // 1.3
+@include_once(WP_PLUGIN_DIR . "/gravityforms/form_display.php"); // 1.3
+$Forms = new RGForms();  $RG = new RGFormsModel(); 
+if(class_exists(GFCommon)) { $Common = new GFCommon(); } else { $Common = false; }
+if(class_exists(GFFormDisplay)) { $FormDisplay = new GFFormDisplay(); } else { $FormDisplay = false; }
 
 // If Gravity Forms is installed and exists
 if(class_exists(RGForms) && class_exists(RGFormsModel)) { 
@@ -78,8 +85,10 @@ if(class_exists(RGForms) && class_exists(RGFormsModel)) {
 	
 <?php 	
 	function kws_gf_head() {
-		kws_gf_css();
-		kws_gf_js();
+		if(is_admin()) {
+			kws_gf_css();
+			kws_gf_js();
+		}
 	}
 	
 	add_action('admin_head', 'kws_gf_head',1);
@@ -88,7 +97,7 @@ if(class_exists(RGForms) && class_exists(RGFormsModel)) {
 	// To retrieve textarea inputs from a lead 
 	// Example: get_gf_field_value_long(22, '14');
 	function get_gf_field_value_long($leadid, $fieldid) {
-		global $Forms, $RG;
+		global $Forms, $RG, $Common;
 		return $RG->get_field_value_long($leadid, $fieldid);
 	}
 	
@@ -99,7 +108,7 @@ if(class_exists(RGForms) && class_exists(RGFormsModel)) {
 	// Gives you the label for a form input (such as First Name). Enter in the form and the field ID to access the label.
 	// Example: echo get_gf_field_label(1,1.3);
 	function get_gf_field_label($form_id, $field_id) {
-		global $Forms,$RG;
+		global $Forms,$RG,$Common;
 		$form = $RG->get_form_meta($form_id);
 		foreach($form["fields"] as $field){
 			if($field['id'] == $field_id) {
@@ -107,7 +116,11 @@ if(class_exists(RGForms) && class_exists(RGFormsModel)) {
 			}elseif(is_array($field['inputs'])) {
 				foreach($field["inputs"] as $input){
 					if($input['id'] == $field_id) {
-						$output = $Forms->escape_text($Forms->get_label($field,$field_id));
+						if($Common) {
+							$output = esc_html($Common->get_label($field,$field_id));
+						} else {
+							$output = $Forms->escape_text($Forms->get_label($field,$field_id));
+						}
 					}
 				}
 			}
@@ -119,18 +132,21 @@ if(class_exists(RGForms) && class_exists(RGFormsModel)) {
 	}	
 	
 	// Returns a form using php instead of shortcode
-	function get_gf_form($id, $display_title=true, $display_description=true){
-		global $Forms,$RG;
-		
-		return $Forms->get_form($id, $display_title, $display_description);
+	function get_gf_form($id, $display_title=true, $display_description=true, $force_display=false, $field_values=null){
+		global $Forms,$RG,$Common,$FormDisplay;
+		if($FormDisplay) {	
+			return $FormDisplay->get_form($id, $display_title=true, $display_description=true, $force_display=false, $field_values=null);
+		} else {
+			return $RG->get_form($id, $display_title, $display_description);
+		}
 	}
-	function gf_form($id, $display_title=true, $display_description=true){
-		echo get_gf_form($id, $display_title, $display_description);
+	function gf_form($id, $display_title=true, $display_description=true, $force_display=false, $field_values=null){
+		echo get_gf_form($id, $display_title, $display_description, $force_display, $field_values);
 	}
 	
 	// Returns array of leads for a specific form
 	function get_gf_leads($form_id, $sort_field_number=0, $sort_direction='DESC', $search='', $offset=0, $page_size=3000, $star=null, $read=null, $is_numeric_sort = false, $start_date=null, $end_date=null) {
-		global $Forms, $RG,$post;
+		global $Forms, $RG,$Common;
 		
 		return $RG->get_leads($form_id,$sort_field_number, $sort_direction, $search, $offset, $page_size, $star, $read, $is_numeric_sort, $start_date, $end_date);
 	}
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Tags: gravity forms, forms, gravity, form, crm, gravity form
 Requires at least: 2.8
 Tested up to: 2.9
-Stable tag: 1.1
+Stable tag: 1.2
 Contributors: katzwebdesign
 Donate link:https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=zackkatz%40gmail%2ecom&item_name=Gravity%20Forms%20Addons&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
 
@@ -12,15 +12,15 @@ Add functionality to the great Gravity Forms plugin.
 
 Requires the [Gravity Forms plugin](http://sn.im/gravityforms) __Don't use Gravity Forms? [Buy the plugin](http://sn.im/gravityforms)__
 
-###Improve Gravity Forms Functionality and Usability
+=== Improve Gravity Forms Functionality and Usability === 
 
-#### Gravity Forms Addons
+### Gravity Forms Addons
 
 * Expand the Add Fields boxes to view all the boxes at once.
 * Edit form entries directly from the Entries page (saving two clicks)
 * Easily access form data to use in your website with PHP functions - [Learn more on the plugin's website](http://www.seodenver.com/gravity-forms-addons/)
 
-#### Upcoming Features
+### Upcoming Features
 
 * Submit forms using Ajax
 
@@ -34,6 +34,9 @@ Developed by Katz Web Services, a __[Denver SEO firm](http://www.katzwebservices
 2. This plugin adds an Edit link to Gravity Form entries
 
 == Changelog ==
+
+= 1.2 = 
+* Compatibility with Gravity Forms 1.3
 
 = 1.1 =
 * Added Edit link to Entries page to directly edit an entry
